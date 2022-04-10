ae241c77fd326361215ca8ee2b4370a9bcdd9495
lucianochapa <chapa.luci@gmail.com>
2022-04-10 13:11:36 -0300

    Início da função ludecomp

--- functions/lu_decomp.py
+++ functions/lu_decomp.py
@@ -1,5 +1,16 @@
-# # Decomposição de matriz LU
-# def decompLU():
-#     return 
+# Decomposição de matriz LU
+def decompLU():
+    n = int(input("Digite o tamanho n da matriz (n x n): "))
+    a = []
+    for i in range(0,n):
+        linha = []
+        print("Linha %i"%(i+1))
+        for j in range(0,n):
+            elem = int(input("A(%i,%i): "%(i+1,j+1)))
+            linha.append(elem)
+        print(67 * "-")
+        a.append(linha)
+    print("Matriz A: %a"%(a))
+    # return
 
-print("Script para LU decomp")
+decompLU()
