8be27b491cddcef7d1e953eda279eb3ff769d661
lucianochapa <chapa.luci@gmail.com>
2022-04-15 23:11:34 -0300

    Tridiagonal padrão finalizado

--- custom_functions.py
+++ custom_functions.py
@@ -1,5 +1,20 @@
 import numpy as np
 
+def recebeA():                  # Pede ao usuário os parâmetros da matriz A a ser decomposta
+    '''Recebe do teclado do usuário o tamanho 'n' da matriz A e seus elementos um a um.
+    
+    Retorna
+    ====
+    A: ndarray
+        Matriz formada pelas inserções do usuário
+        '''
+    n = int(input("Digite o tamanho n da matriz (quadrada, n x n): "))      # Recebe o tamanho da matriz (quadrada) n por n
+    A = []                                                                  # Cria lista vazia para receber os valores
+    for i in range(0,n):                                                    # Itera para o número 'n' de linhas
+        print("Linha %i"%(i+1))                                             # Mostra ao usuário a linha atual
+        A.append([int(input("A(%i,%i): "%(i+1,j+1))) for j in range(0,n)])  # Pede e concatena à lista o próximo valor
+    return np.array(A, float)                                               # Devolve a matriz 'A' como np.ndarray
+
 def decompLU(A: np.ndarray):    # Recebe uma matriz A (ndarray) e executa sua decomposição LU
     '''Recebe uma matriz A e devolve sua decomposição LU
     
@@ -11,91 +26,166 @@ def decompLU(A: np.ndarray):    # Recebe uma matriz A (ndarray) e executa sua de
     Retorna
     ===
     tuple [
-        L: ndarray - matriz inferior,
-        U: ndarray - matriz superior
-            ]
+        L: ndarray
+            matriz inferior
+        U: ndarray
+            matriz superior
+    ]
     '''
-    print("Matriz A:")                                                          # Imprime a matriz A que foi inserida pelo usuário
-    print(A)                                                                    # Imprime a matriz A que foi inserida pelo usuário
-    n = len(A)                                                                  # Guarda o tamanho da matriz A
-    L = [[0]*n]*n                                                               # Cria matriz L (n x n) com zeros em todas as posições
-    L = np.array(L)                                                             # Transforma a lista em ndarray
+    print("Matriz A:")                                                          # Imprime a matriz 'A' que foi inserida pelo usuário
+    print(A)                                                                    # Imprime a matriz 'A' que foi inserida pelo usuário
+    n = len(A)                                                                  # Guarda o tamanho da matriz 'A'
+    L = np.array([[0]*n]*n, float)                                              # Cria matriz 'L'(n por n) com zeros em todas as posições e transforma a lista em ndarray
     for i in range(0,n):                                                        # Itera para todas as linhas
         L[i,i] = 1                                                              # Todos os elementos da diagonal viram 1
-    U = [[0]*n]*n                                                               # Cria matriz U (n x n) com zeros em todas as posições
-    U = np.array(U)                                                             # Transforma a lista em ndarray
+    U = np.array([[0]*n]*n, float)                                              # Cria matriz 'U'(n por n) com zeros em todas as posições e transforma a lista em ndarray
     for i in range(0,n):                                                        # Itera para todas as linhas
         U[i,i:] = A[i,i:] - np.dot(L[i,:i],U[:i,i:])                            # EXPRESSÃO (1) DO EXERCÍCIO
         L[(i+1):,i] = (1/U[i,i])*(A[(i+1):,i] - np.dot(L[(i+1):,:i],U[:i,i]))   # EXPRESSÃO (2) DO EXERCÍCIO
-    print("Matriz inferior L: ")                                                # Imprime a matriz L (inferior)
-    print(L)                                                                    # Imprime a matriz L (inferior)
-    print("Matriz superior U: ")                                                # Imprime a matriz U (superior)
-    print(U)                                                                    # Imprime a matriz U (superior)
-    return L,U                                                                  # Retorna as matrizes L e U como tupla
+    print("Matriz inferior L: ")                                                # Imprime a matriz 'L' (inferior)
+    print(L)                                                                    # Imprime a matriz 'L' (inferior)
+    print("Matriz superior U: ")                                                # Imprime a matriz 'U' (superior)
+    print(U)                                                                    # Imprime a matriz 'U' (superior)
+    return L,U                                                                  # Retorna as matrizes 'L,U' como tupla [np.ndarray, np.ndarray]
 
-def recebeA():                  # Pede ao usuário os parâmetros da matriz A a ser decomposta
-    '''Recebe do teclado do usuário o tamanho 'n' da matriz A e seus elementos um a um.
+def decompLUabc(a: np.ndarray, b: np.ndarray, c: np.ndarray):   # Recebe os vetores diagonais a,b,c e decompõe em vetores l e u
+    '''Realiza a decomposição LU a partir dos 3 vetores diagonais
     
-    Retorna
-    ====
-    A: ndarray
-        Matriz formada pelas inserções do usuário
-        '''
-    n = int(input("Digite o tamanho n da matriz (quadrada, n x n): "))      # Recebe o tamanho da matriz (quadrada) n x n
-    A = []                                                                  # Cria lista vazia para receber os valores
-    for i in range(0,n):                                                    # Itera para o número n de linhas
-        print("Linha %i"%(i+1))                                             # Mostra ao usuário a linha atual
-        A.append([int(input("A(%i,%i): "%(i+1,j+1))) for j in range(0,n)])  # Pede e concatena à lista o próximo valor
-    return np.array(A)                                                      # Devolve a matriz como np.ndarray
+    Parâmetros
+    ===
+    a: ndarray
+        Vetor subdiagonal
+    b: ndarray
+        Vetor diagonal
+    c: ndarray
+        Vetor sobrediagonal
 
-def solveTridi(a: np.ndarray, b: np.ndarray, c: np.ndarray, d: np.ndarray):
-    '''Resolve um sistema linear tridiagonal A*x = d utilizando uma decomposição LU --> LUx = d
+    Retorna
+    ===
+    tuple [
+        l: ndarray
+            Vetor subdiagonal da matriz inferior
+        u: ndarray
+            Vetor diagonal da matriz superior
+    ]
+    '''
+    n = len(b)                              # Guarda o valor do vetor diagonal
+    l = np.array([0]*(n-1), float)          # Cria um vetor vazio com zero em todas as posições, de tamanho n-1
+    u = np.array([0]*n, float)              # Cria um vetor vazio com zero em todas as posições, de tamanho n
+    u[0] = b[0]                             # Primeiro termo do vetor 'u'
+    for i in range(1,n):                    # Itera ao longo do tamanho do vetor diagonal
+        l[i-1] = a[i]/u[i-1]                # Expressão do exercício
+        u[i] = b[i] - l[i-1]*c[i-1]         # Expressão do exercício
+    return l, u                             # Retorna os vetores 'l,u' como tupla [np.ndarray, np.ndarray]
+
+def solveLydUxy(l,u,c,d):   # Resolve um sistema tridiagonal a partir de Ly = d, Ux = y
+    '''Recebe os vetores subdiagonal da matriz inferior, diagonal e sobrediagonal da matriz superior, termos independentes e retorna a solução
+
+    Parâmetros
+    ===
+    L: ndarray
+        Vetor subdiagonal da matriz inferior
+    U: ndarray
+        Vetor diagonal da matriz superior
+    c: ndarray
+        Vetor sobrediagonal da matriz superior
+    d: ndarray
+        Vetor de termos independentes
+
+    Retorna
+    ===
+    x: ndarray
+        Vetor de soluções do sistema
+    '''
+    n = len(d)                              # Guarda o tamanho do sistema
+    y = np.array([0]*n, float)              # Cria um vetor 'y' com zero em todas as posições
+    y[0] = d[0]                             # Primeiro termo de 'y'
+    for i in range(1,n):                    # Itera para o tamanho do sistema
+        y[i] = d[i] - l[i-1]*y[i-1]         # Expressão do exercício
+    x = np.array([0]*n, float)              # Cria um vetor 'x' com zero em todas as posições
+    x[n-1] = y[n-1]/u[n-1]                  # Último termo de 'x'
+    for i in range(n-2,-1,-1):              # Itera para o tamanho do sistema, do último para o primeiro termo
+        x[i] = (y[i] - c[i]*x[i+1])/u[i]    # Expressão do exercício
+    return x                                # Retorna 'x' como np.ndarray
+
+def A2abc(A: np.ndarray):   # Transforma uma matriz A tridiagonal em 3 vetores
+    '''Recebe uma matriz tridiagonal e a transforma nos 3 vetores diagonais.
+
+    Parâmetros
+    ===
+    A: ndarray
+        Matriz tridiagonal
+
+    Retorna
+    ===
+    tuple [
+        a: ndarray
+            Subdiagonal de A
+        b: ndarray
+            Diagonal de A
+        c: ndarray
+            Sobrediagonal de A
+    ]
+    '''
+    n = len(A)                  # Guarda o tamanho da matriz
+    a = []                      # Cria lista vazia
+    b = []                      # Cria lista vazia
+    c = []                      # Cria lista vazia
+    for i in range(1,n):        # Itera para o tamanho da matriz
+        a.append(A[i,i-1])      # Cria o vetor subdiagonal 'a'
+        b.append(A[i-1,i-1])    # Cria o vetor diagonal 'b'
+        c.append(A[i-1,i])      # Cria o vetor sobrediagonal 'c'
+    b.append(A[n-1,n-1])        # Guarda o último valor do vetor diagonal 'b'
+    a = np.array(a)             # Transforma a lista em np.ndarray
+    b = np.array(b)             # Transforma a lista em np.ndarray
+    c = np.array(c)             # Transforma a lista em np.ndarray
+    return a, b, c              # Retorna os valores 'a,b,c' como tupla [np.ndarray, np.ndarray, np.ndarray]
+
+def abc2A(a: np.ndarray, b: np.ndarray, c: np.ndarray): # Transforma 3 vetores diagonais em uma matriz tridiagonal
+    '''Recebe 3 vetores diagonais e os transforma numa matriz tridiagonal.
 
     Parâmetros
     ===
     a: ndarray
-        Vetor contendo a subdiagonal dos coeficientes de X, de tamanho n
+        Subdiagonal de A
     b: ndarray
-        Vetor contendo a diagonal dos coeficientes de X, de tamanho n
+        Diagonal de A
     c: ndarray
-        Vetor contendo a sobrediagonal dos coeficientes de X, de tamanho n
+        Sobrediagonal de A
+
+    Retorna
+    ===
+    A: ndarray
+        Matriz tridiagonal
+    '''
+    n = len(b)                          # Guarda o tamanho do vetor diagonal
+    A = np.array([[0]*n]*n, float)      # Cria matriz 'A'(n por n) com zero em todas as posições
+    for i in range(0,n-1):              # Itera para o tamanho do vetor diagonal
+        A[i+1,i] = a[i]                 # Recebe valores do vetor subdiagonal 'a'
+        A[i,i] = b[i]                   # Recebe valores do vetor diagonal 'b'
+        A[i,i+1] = c[i]                 # Recebe valores do vetor sobrediagonal 'c'
+    A[n-1,n-1] = b[n-1]                 # Recebe o último valor do vetor diagonal 'b'
+    return A                            # Retorna a matriz 'A' como np.ndarray
+
+def solveTridi(a: np.ndarray, b: np.ndarray, c: np.ndarray, d: np.ndarray): # Recebe vetores diagonais e independente e devolve a resolução
+    '''Resolve um sistema linear tridiagonal A*x = d utilizando uma decomposição LU: Ly = d, Ux = y
+
+    Parâmetros
+    ===
+    a: ndarray
+        Vetor contendo a subdiagonal dos coeficientes de X
+    b: ndarray
+        Vetor contendo a diagonal dos coeficientes de X
+    c: ndarray
+        Vetor contendo a sobrediagonal dos coeficientes de X
     d: ndarray
-        Vetor dos coeficientes independentes do sistema, de tamanho n
+        Vetor dos coeficientes independentes do sistema
+
+    Retorna
+    ===
+    x: ndarray
+        Vetor de soluções para X do sistema
     '''    
-    a = np.array(a)
-    b = np.array(b)
-    c = np.array(c)
-    d = np.array(d)
-    L, U = tridiDecompLU(a,b,c)
-    x = solveLUxd(L,U,c,d)
-    return x
-
-def tridiDecompLU(a,b,c):
-    n = len(b)
-    L = [0]*(n-1)
-    U = [0]*n
-    L = np.array(L)
-    U = np.array(U)
-
-    U[0]=b[0]
-    for i in range(1,n):
-        L[i] = a[i]/U[i-1]
-        U[i] = b[i] - L[i]*c[i-1]
-    
-    # print(L)
-    # print(U)
-
-    return L, U
-
-def solveLUxd(L,U,c,d):
-    n = len(d)
-    y = [0]*n
-    y[0] = d[0]
-    for i in range(1,n):
-        y[i] = d[i] - L[i]*y[i-1]
-    x = [0]*n
-    x[n-1] = y[n-1]/U[n-1]
-    for i in range(n-2,-1,-1):
-        x[i] = (y[i] - c[i]*x[i+1])/U[i]
-    x = np.array(x)
-    return x
+    l, u = decompLUabc(a,b,c)   # Decompõe 3 vetores 'a,b,c' de um sistema tridiagonal em vetores 'l' (subdiagonal inferior) e 'u' (diagonal superior)
+    x = solveLydUxy(l,u,c,d)    # Usa os vetores 'l' (subdiagonal inferior), 'u' (diagonal superior), 'c' (sobrediagonal superior), 'd' (termos independentes) e resolve o sistema para X
+    return x                    # Retorna um vetor de soluções
--- main.py
+++ main.py
@@ -31,26 +31,26 @@ def printMenu(options: dict, name:str):
     return options                                                      # Retorna o dict 'options'
 
 def main():
-    while True:                                                                             
-        menu = {                                                                            # Cria um menu de opções como dict
+    while True:                                                                                     # Abre um laço e um nível de menu
+        options = {                                                                                 # Cria um menu de opções como dict
             1: 'Decomposição LU de matriz',
             2: 'Resolução de sistema tridiagonal usando decomposição de matriz LU'
             }
-        options = printMenu(options=menu, name="MENU")                                      # Imprime o menu e retorna um dict com as opções
-        choice = int(input("Digite o número da ação desejada (1 a %i): "%(len(options))))   # Recebe do usuário uma ação do menu (int)
-        try: options[choice]                                                                # Verifica se a opção é válida
-        except KeyError:                                                                    # Se não for, mantém o laço e retorna ao menu
-            print("Opção não disponível. Escolha a ação desejada do menu abaixo: ")         # Imprime mensagem de erro
-        else:                                                                               # Se for válida
-            print("Opção escolhida: %s"%(options[choice]))                                  # Imprime a descrição da ação escolhida
-            if(choice == 1):                                                                # Se escolheu decomp LU
-                while True:
-                    entradas = {                                                            # Cria um menu de opções como dict
+        options = printMenu(options=options, name="MENU PRINCIPAL")                                 # Imprime o menu e retorna um dict com as opções
+        choice = int(input("Digite o número da ação desejada (1 a %i): "%(len(options))))           # Recebe do usuário uma ação do menu (int)
+        try: options[choice]                                                                        # Verifica se a opção é válida
+        except KeyError:                                                                            # Se não for, mantém o laço e retorna ao menu
+            print("Opção não disponível. Escolha a ação desejada do menu abaixo: ")                 # Imprime mensagem de erro
+        else:                                                                                       # Se for válida
+            print("Opção escolhida: %s"%(options[choice]))                                          # Imprime a descrição da ação escolhida
+            if(choice == 1):                                                                        # Se escolheu decomp LU
+                while True:                                                                         # Abre um laço e um nível de menu
+                    options = {                                                                     # Cria um menu de opções como dict
                         1: 'Digitar elementos um a um',
                         2: 'Escolher de um arquivo',
                         3: 'Voltar'
                         }
-                    options = printMenu(options=entradas, name="MÉTODO DE ENTRADA")                 # Imprime o menu e retorna um dict com as opções
+                    options = printMenu(options=options, name="MÉTODO DE ENTRADA")                  # Imprime o menu e retorna um dict com as opções
                     choice = int(input("Escolha um método de entrada (1 a %i): "%(len(options))))   # Recebe do usuário uma ação do menu (int)
                     print("Opção escolhida: %s"%(options[choice]))                                  # Imprime a descrição da ação escolhida
                     try: options[choice]                                                            # Verifica se a opção é válida
@@ -60,18 +60,65 @@ def main():
                         if(choice == 1):                                                            # Se escolheu entrada manual
                             A = cf.recebeA()                                                        # Roda função para receber os valores um a um
                         elif(choice == 2):                                                          # Se escolheu entrada por arquivo
-                            A = np.genfromtxt('matriz.csv', delimiter=',')                          # Importa os valores do csv para um ndarray
+                            # Script para importar a matriz A de um csv
+                            A = np.genfromtxt('matriz2.csv', delimiter=',')                         # Importa os valores do csv para um ndarray
                         elif(choice == 3):                                                          # Se escolheu voltar ao menu
                             break                                                                   # Quebra o laço e volta ao menu anterior
                         else:                                                                       # Se escolheu encerrar o programa
                             return                                                                  # Encerra o programa
                         cf.decompLU(A)                                                              # Executa script de decomposição LU
                         break                                                                       # Quebra o laço e volta ao menu anterior
-            elif(choice == 2):                                                                      # Se escolheu sistema tridi
-                a = [0,-1,-1,-1]                                                                    # Subdiagonal da matriz de coeficientes
-                b = [2,2,2,2]                                                                       # Diagonal da matriz de coeficientes
-                c = [-1,-1,-1,0]                                                                    # Sobrediagonal da matriz de coeficientes
-                d = [1,0,0,1]                                                                       # Coeficientes independentes do sistema
+            elif(choice == 2):                                                                      # Se escolheu sistema tridiagonal
+                while True:                                                                         # Abre um laço e um nível de menu
+                    options = {                                                                     # Cria um menu de opções como dict
+                        1: 'Inserir a matriz tridiagonal \'A\' dos coeficientes de X',
+                        2: 'Inserir os vetores diagonais \'a,b,c\' da matriz tridiagonal dos coeficientes de X',
+                        3: 'Voltar'
+                        }
+                    options = printMenu(options=options, name="MÉTODO DE ENTRADA")                  # Imprime o menu e retorna um dict com as opções
+                    choice = int(input("Escolha um método de entrada (1 a %i): "%(len(options))))   # Recebe do usuário uma ação do menu (int)
+                    print("Opção escolhida: %s"%(options[choice]))                                  # Imprime a descrição da ação escolhida
+                    try: options[choice]                                                            # Verifica se a opção é válida
+                    except KeyError:                                                                # Se não for, mantém o laço e retorna ao menu
+                        print("Opção não disponível. Escolha um método do menu abaixo: ")           # Imprime mensagem de erro
+                    else:                                                                           # Se for válida
+                        if(choice == 1):                                                            # Se escolheu matriz de coeficientes
+                            # Script para receber uma matriz de coeficientes (quebrar em inserção manual e importação)
+                            A = []
+                        elif(choice == 2):                                                          # Se escolheu vetores diagonais
+                            # Script para receber vetores diagonais (quebrar em inserção manual e importação)
+                            a = np.array([0,-1,-1,-1])                                              # Subdiagonal da matriz de coeficientes
+                            b = np.array([2,2,2,2])                                                 # Diagonal da matriz de coeficientes
+                            c = np.array([-1,-1,-1,0])                                              # Sobrediagonal da matriz de coeficientes
+                        elif(choice == 3):                                                          # Se escolheu voltar ao menu
+                            break                                                                   # Quebra o laço e volta ao menu anterior
+                        else:                                                                       # Se escolheu encerrar o programa
+                            return                                                                  # Encerra o programa
+                        while True:
+                            options = {                                                                     # Cria um menu de opções como dict
+                                1: 'Digitar o vetor \'d\' de termos independetes',
+                                2: 'Escolher de um arquivo',
+                                3: 'Voltar'
+                                }
+                            options = printMenu(options=options, name="MÉTODO DE ENTRADA")                  # Imprime o menu e retorna um dict com as opções
+                            choice = int(input("Escolha um método de entrada (1 a %i): "%(len(options))))   # Recebe do usuário uma ação do menu (int)
+                            print("Opção escolhida: %s"%(options[choice]))                                  # Imprime a descrição da ação escolhida
+                            try: options[choice]                                                            # Verifica se a opção é válida
+                            except KeyError:                                                                # Se não for, mantém o laço e retorna ao menu
+                                print("Opção não disponível. Escolha um método do menu abaixo: ")           # Imprime mensagem de erro
+                            else:                                                                           # Se for válida
+                                if(choice == 1):                                                            # Se escolheu digitar manualmente
+                                    # Rodar script para receber do teclado do usuário
+                                    A = []
+                                elif(choice == 2):                                                          # Se escolheu importar
+                                    # Rodar script para importar de csv
+                                    d = np.array([1,0,0,1])                                                 # Coeficientes independentes do sistema
+                                elif(choice == 3):                                                          # Se escolheu voltar ao menu
+                                    break                                                                   # Quebra o laço e volta ao menu anterior
+                                else:                                                                       # Se escolheu encerrar o programa
+                                    return
+                                break
+                        break                                                                       # Quebra o laço e volta ao menu anterior
                 x = cf.solveTridi(a,b,c,d)                                                          # Executa script de resolução de sistema tridiagonal e guarda resultado em 'x'
                 print(x)                                                                            # Imprime 'x'
             else:                                                                                   # Se o usuário escolheu sair
